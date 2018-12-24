43992dd1f21d529d57ce49ef03259f6ae6e2533a
blank11 <blank7lee@gmail.com>
2018-12-24 15:02:25 +0800

    modify cmap args and add commits

--- plot_confuse_matrix.py
+++ plot_confuse_matrix.py
@@ -2,7 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 # %matplotlib inline # ipynb
 
-def plot_confuse_matrix(cm, classes, normalized=False, title='Confuse Matrix', plt.cm.Blues):
+def plot_confuse_matrix(cm, classes, normalized=False, title='Confuse Matrix', cmap=plt.cm.Blues):
+	"""
+	Args:
+		cm: confuse matrix
+		classes: labels of y
+		normalized: whether confuse matrix is normalized
+		title: plot title
+		cmap: color map
+
+	Returns:
+		Print confuse matrix, and plot confuse matrix
+	"""
 	if normalized:
 		cm = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
 		print('Normalized confuse matrix')
@@ -11,10 +22,10 @@ def plot_confuse_matrix(cm, classes, normalized=False, title='Confuse Matrix', p
 
 	print(cm)
 
-	plt.imshow(cm, interplation='nearest', cmp=cmp)
+	plt.imshow(cm, interplation='nearest', cmap=cmap)
 	plt.title(title)
-	plt.colorbar()
-	tick_marks = np.arrange(len(classes))
+	plt.colorbar() # create color bar
+	tick_marks = np.arrange(len(classes)) # generate number of classes
 	plt.yticks(tick_marks, classsed, rotation=45)
 	plt.xticks(tick_marks, classes)
 
@@ -22,6 +33,7 @@ def plot_confuse_matrix(cm, classes, normalized=False, title='Confuse Matrix', p
 	thresh = cm.max / 2.
 
 	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+		# itertools.product: equivalent to for i, j in [x, y]
 		plt.text(j, i, format(cm[i, j], fmt), horizentalalignment='center', color='white' if cm[i, j] > thresh else 'black')
 
 	plt.tight_layout()
